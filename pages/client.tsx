31237ac72fd9013726c374ec8dac33a26f7bf0d9
LongKC <longkc@itsol.vn>
2023-04-25 13:47:31 +0700

    dev'

--- pages/client.tsx
+++
@@ -1,7 +0,0 @@
-export default function Client({ source }: any) {
-    return (
-        <div className="wrapper">
-            {/*<MDXRemote {...source} components={components} />*/}
-        </div>
-    )
-}
