087ce0ae03ca3969ae5f36765596db13603e838a
LongKC <longkc@itsol.vn>
2023-04-26 10:12:23 +0700

    dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { remarkCodeHike } from "@code-hike/mdx"
 import { CH } from '@code-hike/mdx/components'
 import theme from "shiki/themes/material-default.json"
 import { serialize } from 'next-mdx-remote/serialize'
+import remarkHint from "remark-hint";
+import remarkToc from "remark-toc";
+import remarkRehype from "remark-rehype";
 
 const components = { CH }
 
@@ -17,7 +20,34 @@ export default function TestPage({ source }: any) {
 export async function getStaticProps() {
     // Server side
     // MDX text - can be from a local file, database, anywhere
-    const source = '<CH.Code>\n' +
+    const source = '' +
+        '# Hello world \n' +
+        '__Xin chao cac ban__ \n' +
+        '## Table of Contents \n' +
+
+        '## Hello 1 \n' +
+        '__Xin chao cac ban__ \n' +
+        '## Hello 2 \n' +
+        '### Hello 2.1 \n' +
+        '### Hello 2.2 \n' +
+        '#### Hello 2.2.1 \n' +
+        '#### Hello 2.2.2 \n' +
+        '##### Hello 2.2.2.1 \n' +
+        '##### Hello 2.2.2.2 \n' +
+        '!> Here is a tip.\n' +
+        '\n' +
+        '?> And a warning.\n' +
+        '\n' +
+        'x> Or an error. \n' +
+        '<input id="input_1" className="input_clazz"/>' +
+        '\n' +
+        '## World 1 \n' +
+        '## World 2 \n' +
+        '### World 2.1 \n' +
+        '### World 2.2 \n' +
+        '> Quote  \n' +
+        '> Quote2  \n' +
+        '<CH.Code>\n' +
         '\n' +
         '```js app.js\n' +
         'function lorem(ipsum, dolor = 1) {\n' +
@@ -38,7 +68,12 @@ export async function getStaticProps() {
         '</CH.Code>'
     const mdxSource = await serialize(source, {
         mdxOptions: {
-            remarkPlugins: [[remarkCodeHike, {autoImport: false, theme}]],
+            remarkPlugins: [
+                [remarkCodeHike, {autoImport: false, theme}],
+                [remarkHint],
+                [remarkToc, {maxDepth: 3, ordered: true}],
+                [remarkRehype]
+            ],
             useDynamicImport: true,
         },
     })
